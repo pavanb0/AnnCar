d024f8391c2fd5b5136065e9087a273ff7dc282a
pavanb0 <pavanbagwe16@gmail.com>
2024-05-05 21:12:42 +0530

    edge detection

--- training/testAnn.py
+++ training/testAnn.py
@@ -1,6 +1,7 @@
 from keras.models import load_model
 import cv2
 import numpy as np
+import time
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -14,40 +15,40 @@ class_names = open("training\labels.txt", "r").readlines()
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture("http://192.168.0.100:8080/video")
 
-frame_counter = 0  # Counter to track frames
+capture_interval = 3  # Capture an image every 3 seconds
+start_time = time.time()
 
 while True:
-    # Grab the web camera's image.
-    ret, image = camera.read()
+    # Calculate elapsed time since the last capture
+    elapsed_time = time.time() - start_time
 
-    # Increment frame counter
-    frame_counter += 1
+    if elapsed_time >= capture_interval:
+        start_time = time.time()  # Reset the timer
 
-    # Skip processing if it's an odd-numbered frame
-    if frame_counter % 2 != 0:
-        continue
+        # Grab the web camera's image.
+        ret, image = camera.read()
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
 
-    # Make the image a numpy array and reshape it to the model's input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Make the image a numpy array and reshape it to the model's input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
+        # Normalize the image array
+        image = (image / 127.5) - 1
 
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+        # Predicts the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
